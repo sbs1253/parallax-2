10c6e867af0111b32942c84b9f40be431d12492f
songbbang <rokaf340@gmail.com>
2023-07-09 20:32:37 +0900

    transition

--- 3.chapter/js/index.js
+++ 3.chapter/js/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-loop-func */
 /* eslint-disable no-plusplus */
 /* eslint-disable prefer-const */
@@ -7,6 +8,7 @@ window.onload = () => {
   let prevButton = document.querySelectorAll('button')[0];
   let nextButton = document.querySelectorAll('button')[1];
   let contentWrap = document.querySelector('.contentWrap');
+  let gradientWrap = document.querySelector('.gradientWrap');
   let diskInner = document.querySelectorAll('.disk_inner');
   let album = document.querySelectorAll('.album');
   let pointBtnAll = document.querySelectorAll('.pointWrap li');
@@ -16,26 +18,33 @@ window.onload = () => {
   bgArray[0] = ['#0272a4', '#f6a564'];
   bgArray[1] = ['#b6bfc8', '#36595b'];
   bgArray[2] = ['#e58e82', '#6f569f'];
-
+  function color() {
+    gradientWrap.style.opacity = 1;
+  }
   function pageChangeFunc() {
-    console.log(`totalNum = ${totalNum} pageNum = ${pageNum}`);
+    gradientWrap.style.cssText = `background:linear-gradient(120deg, ${bgArray[pageNum][0]}, ${bgArray[pageNum][1]});
+    opacity : 0`;
+    color();
 
-    contentWrap.computedStyleMap.background = `linear-gradient(120deg, ${
-      (bgArray[pageNum][0], bgArray[pageNum][1])
-    })`;
-
+    // contentWrap.style.background =
+    //   'linear-gradient(120deg,' +
+    //   bgArray[pageNum][0] +
+    //   ', ' +
+    //   bgArray[pageNum][1] +
+    //   ')';
     for (let i = 0; i < totalNum; i++) {
       if (pageNum === i) {
         // 현재 페이지
+
         album[i].classList.add('active');
         pointBtnAll[i].classList.add('active');
       } else {
         // console.log(i);
-
         album[i].classList.remove('active');
         pointBtnAll[i].classList.remove('active');
       }
     }
+
     diskInner[pageNum].style.background = bgArray[pageNum];
   }
   // 최초실행
