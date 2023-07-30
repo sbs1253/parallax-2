b0c925e5403713b6b4166f35ec1d67bdb5678cef
songbbang <rokaf340@gmail.com>
2023-07-30 19:22:54 +0900

    3D

--- rotate_3D/js/index.js
+++ rotate_3D/js/index.js
@@ -2,8 +2,8 @@
 window.onload = () => {
   const child = document.querySelector('.child');
   for (let i = 0; i < 19; i++) {
-    const content = document.createElement('div');
-    content.innerHTML = 'hi';
+    const content = document.createElement('span');
+    content.classList.add('content');
     child.appendChild(content);
   }
 };
